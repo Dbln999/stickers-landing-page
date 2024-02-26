73aed586ad870279793fda2b15731abff5c01d22
danii <daniil3221337@gmail.com>
2024-02-26 10:23:21 +0200

    email validation

--- src/pages/PaymentPage/PaymentPage.tsx
+++ src/pages/PaymentPage/PaymentPage.tsx
@@ -79,7 +79,8 @@ const PaymentPage = () => {
   useEffect(() => {
     if (
       !form?.name ||
-      !form.email ||
+      (!form.email &&
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(form.email!)) ||
       !form.phoneNumber ||
       !form.postcode ||
       !form.address ||
