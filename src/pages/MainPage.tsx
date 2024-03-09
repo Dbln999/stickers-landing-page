ce699fd1a4e98b0ea5de9781b79eb54e68122f59
Dbln999 <daniil3221337@gmail.com>
2024-03-09 14:58:17 +0200

    redesign

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import Header from "../widget/Header/Header.tsx";
 import { MainContent } from "../widget/MainContent/MainContent.tsx";
-import { Footer } from "../widget/Footer/Footer.tsx";
+// import { Footer } from "../widget/Footer/Footer.tsx";
 import cls from './MainPage.module.css'
 const MainPage = () => {
   return (
