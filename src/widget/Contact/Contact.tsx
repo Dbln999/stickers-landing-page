5c8c5ab08f20f0b122b268fe82bc34e195daba2e
Dbln999 <daniil3221337@gmail.com>
2024-03-11 20:21:59 +0200

    email sending

--- src/widget/Contact/Contact.tsx
+++ src/widget/Contact/Contact.tsx
@@ -35,7 +35,6 @@ const Contact = memo(forwardRef((props, ref: ForwardedRef<HTMLHeadingElement>) =
         [dispatch]
     );
     const sendMessage = async () => {
-        console.log('trigger', message)
         await axios.post("https://9e112ff4-7dff-4e62-89f8-db81fb7d5d88-00-1r5bc502c5svq.worf.replit.dev/email/send", message)
     }
 
