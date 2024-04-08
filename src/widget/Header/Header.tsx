8d579e3c9b89b93fa616491924700ba324a9ea1e
Dbln999 <daniil3221337@gmail.com>
2024-04-08 22:45:18 +0300

    hovers + animations

--- src/widget/Header/Header.tsx
+++ src/widget/Header/Header.tsx
@@ -4,7 +4,6 @@ import {useNavigate} from "react-router-dom";
 import Facebook from "../../assets/Facebook.svg";
 import Instagram from "../../assets/Instagram.svg";
 import TikTok from "../../assets/TikTok.svg";
-import YouTube from "../../assets/YouTube.svg";
 import LinkedIn from "../../assets/LinkedIn.svg";
 import {memo} from "react";
 
@@ -30,7 +29,6 @@ const Header = memo(({executeScroll, executeContactScroll}: HeaderProps) => {
                         <img src={Facebook as string} alt="facebook" onClick={() => window.open("https://www.facebook.com/profile.php?id=61556891175713")}/>
                         <img src={Instagram as string} alt="instagram" onClick={() => window.open("https://www.instagram.com/telest1ckers?igsh=MTU1c2FuYjVuaHh6aA==")}/>
                         <img src={TikTok as string} alt="tiktok" onClick={() => window.open("https://www.tiktok.com/@telestickers?_t=8l58vazWZjU&_r=1")}/>
-                        <img src={YouTube as string} alt="youtube"/>
                         <img src={LinkedIn as string} alt="linkedin" onClick={() => window.open("https://www.linkedin.com/company/telestickers/")}/>
                     </div>
                     <div className={cls.links}>
