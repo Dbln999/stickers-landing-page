31d99ccc2a21be282d3e0f10041af5ede50bfd23
Dbln999 <daniil3221337@gmail.com>
2024-04-08 23:05:39 +0300

    hovers + animations

--- src/pages/PaymentPage/PaymentPage.tsx
+++ src/pages/PaymentPage/PaymentPage.tsx
@@ -78,7 +78,6 @@ const PaymentPage = () => {
   useEffect(() => {
     if (
       !form?.name ||
-      !/^[a-zA-Z\s]+$/.test(form.name) ||
       !form.email ||
       !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(form.email!) ||
       !form.phoneNumber ||
