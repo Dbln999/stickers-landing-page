177f604714d983eec6bea63bc701fa99b0be8d73
Dbln999 <daniil3221337@gmail.com>
2024-04-16 20:02:27 +0300

    hovers + animations

--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -5,20 +5,29 @@ import {Button, ButtonSize, ButtonTheme} from "../Button/Button.tsx";
 
 interface StepsProps {
     className?: string;
+    reversed?:boolean;
     image: string
+    phoneImage:string;
     text: string
     button: boolean
 }
 
 const Steps = memo((props: StepsProps) => {
-    const {className, text, button, image} = props
+    const {className, text, button, image, phoneImage, reversed} = props
+
+    const mods: { [p: number]: boolean | undefined } = {
+        [cls.reversed]: reversed,
+    }
     return (
-        <div className={classNames(cls.Steps, {}, [className])}>
-            <img src={image} className={cls.image} alt=""/>
-            <div className={cls.wrapper}>
-                <div className={cls.container}>{text}</div>
-                {button && <Button onClick={() => window.open("https://t.me/Telesticksbot")} size={ButtonSize.XL} className={cls.btn} theme={ButtonTheme.BACKGROUND}>Telegram Bot</Button>}
+        <div className={classNames(cls.Steps, mods, [className])}>
+            <div className={cls.phoneWrapper}>
+                <img src={image} className={cls.image} alt=""/>
+                <div className={cls.wrapper}>
+                    <div className={cls.container}>{text}</div>
+                    {button && <Button onClick={() => window.open("https://t.me/Telesticksbot")} size={ButtonSize.XL} className={cls.btn} theme={ButtonTheme.BACKGROUND}>Telegram Bot</Button>}
+                </div>
             </div>
+                <img className={cls.phone} src={phoneImage} alt=""/>
         </div>
     );
 });
--- src/widget/HowToGetSticker/HowToGetSticker.tsx
+++ src/widget/HowToGetSticker/HowToGetSticker.tsx
@@ -6,15 +6,18 @@ import firstStep from '../../assets/1 step.svg'
 import secondStep from '../../assets/2 step.svg'
 import thirdStep from '../../assets/3 step.svg'
 import Heading from "../../components/Heading/Heading.tsx";
+import stepOnePhone from '../../assets/Phone step 1.svg'
+import stepTwoPhone from '../../assets/Phone step 2.svg';
+import stepThreePhone from '../../assets/Phone step 3.svg';
 
 const HowToGetSticker = memo(forwardRef((props, ref:ForwardedRef<HTMLHeadingElement>) => {
     const {} = props
     return (
         <div className={classNames(cls.HowToGetSticker, {}, [])}>
             <Heading ref={ref} text={"How to get Your custom sticker?"}></Heading>
-            <Steps image={firstStep as string} text={"Go to our Telegram bot, start a conversation and follow the instructions the bot will give you"} button></Steps>
-            <Steps image={secondStep as string} text={"Proceed with payment for the stickers you have selected"} button={false}></Steps>
-            <Steps image={thirdStep as string} text={"Receive the stickers you've ordered"} button={false}></Steps>
+            <Steps phoneImage={stepOnePhone} image={firstStep as string} text={"Go to our Telegram bot, start a conversation and follow the instructions the bot will give you"} button></Steps>
+            <Steps reversed phoneImage={stepTwoPhone} image={secondStep as string} text={"Proceed with payment for the stickers you have selected"} button={false}></Steps>
+            <Steps phoneImage={stepThreePhone} image={thirdStep as string} text={"Receive the stickers you've ordered"} button={false}></Steps>
         </div>
     );
 }));
